772fb2b040d2de9c35a124bf914bd152a6790803
009-dev <54766517+009-dev@users.noreply.github.com>
2019-11-12 18:24:49 +0500

    Add files via upload

---
+++ app/src/main/java/com/waqas/pentagone/TableAvailability.java
@@ -0,0 +1,14 @@
+package com.example.myapplication;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class TableAvailability extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.Table_Availability);
+    }
+}
